377917f9deb7e64721ba342aa8ae4c3b4067fe38
TasmiaTory37 <torytasmmiaakter@gmail.com>
2025-06-05 23:01:04 +0600

    Map Done

---
+++ src/components/Map.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png',
+});
+
+const Map = () => {
+  const position = [40.73061, -73.935242]; 
+
+  return (
+    <div className="w-full h-[400px] rounded-lg overflow-hidden shadow-lg">
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            CozyNest<br /> Green Street, Green City<br />
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Banner from '../components/Banner';
+import Map from '../components/Map';
+
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <section className="mt-16 px-4 max-w-7xl mx-auto">
+                <h2 className="text-3xl text-blue-500 font-bold mb-2 text-center">Our Location</h2>
+                <p className="text-center text-gray-600 mb-6">We’re right where you want to be - at the heart of the city!</p>
+                <Map />
+            </section>
+           
+         
             
         </div>
     );
